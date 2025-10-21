6d2531740ce479136543907bfa5dee92c494f399
Nilqua <savezarzath@gmail.com>
2025-10-21 23:54:06 +0700

    Edit ModelTest.py

--- ModelTest.py
+++ ModelTest.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.impute import SimpleImputer
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import Pipeline
+from sklearn.pipeline import Pipeline, make_pipeline
 from sklearn.linear_model import LinearRegression,LassoCV,RidgeCV,ElasticNetCV
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from sklearn.neural_network import MLPRegressor
@@ -18,19 +18,17 @@ numeric_df = df.select_dtypes(include=["number"])
 cleaned_df = numeric_df.dropna()
 
 # Split data into features and target
-X = cleaned_df.drop(columns=["Calories (kcal)"])
+X = cleaned_df.drop(columns=["Calories (kcal)","Free Sugar (g)","Fibre (g)","Sodium (mg)","Calcium (mg)","Iron (mg)","Vitamin C (mg)","Folate (µg)"])
 y = cleaned_df["Calories (kcal)"]
-
 
 #Testing model
 random_seed = [1,2,3,5,7,99,100,666,2025,9999]
 
 for seed in random_seed:
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=seed)
-    pipeline = Pipeline([
-        ("scaler", StandardScaler()),
-        ("model", LinearRegression()),
-    ])
+    pipeline = make_pipeline(
+        StandardScaler(),
+        LinearRegression(),)
     pipeline.fit(X_train, y_train)
 
     # Make predictions
