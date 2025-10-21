f719bf290186317fa7067406d16fad39657c4313
Nilqua <savezarzath@gmail.com>
2025-10-21 20:16:24 +0700

    Edit ModelTest.py

--- ModelTest.py
+++ ModelTest.py
@@ -13,25 +13,25 @@ df = pd.read_csv("Indian_Food_Nutrition_Processed.csv")
 
 numeric_df = df.select_dtypes(include=["number"])
 
+# Clean data (Drop row that have missing value)
+cleaned_df = numeric_df.dropna()
+
 # Split data into features and target
-X = numeric_df.drop(columns=["Calories (kcal)"])
-y = numeric_df["Calories (kcal)"]
+X = cleaned_df.drop(columns=["Calories (kcal)"])
+y = cleaned_df["Calories (kcal)"]
 
-random_seed = [1,2,3,5,7,99,100,666,2025]
 
-print("DecisionTreeRegressor")
+#Testing model
+random_seed = [1,2,3,5,7,99,100,666,2025,9999]
+
+print("MLPRegressor")
 for seed in random_seed:
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=seed)
-    pipe = Pipeline(steps=[
-        ('imputer', SimpleImputer(strategy='mean')),
-        ('model', DecisionTreeRegressor(max_depth=100,random_state=seed))
-    ])
-    pipe.fit(X_train, y_train)
+    model = MLPRegressor(random_state=seed, hidden_layer_sizes=(15,), max_iter=10000)
+    model.fit(X_train, y_train)
 
     # Make predictions
-    y_pred = pipe.predict(X_test)
-
-    # chosen_l1_ratio = pipe['model'].l1_ratio_
+    y_pred = model.predict(X_test)
 
     mse = mean_squared_error(y_test, y_pred)
     mae = mean_absolute_error(y_test, y_pred)
